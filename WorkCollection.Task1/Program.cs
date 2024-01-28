31954de75399b78b993532358e944a4ea4d07a8d
Marat <milya010790@mail.ru>
2024-01-28 14:12:27 +0500

    Рефакторинг первого задания

--- WorkCollection.Task1/Program.cs
+++ WorkCollection.Task1/Program.cs
@@ -20,45 +20,6 @@ namespace WorkCollection.Task1
 
         }
 
-        static double Estimate<T>(int n,IEnumerable<T> files,Func<IEnumerable<T>,ICollection<T>> func)
-        {
-            var list = new List<long>(n);
-            var timer = new Stopwatch();
-            timer.Start();
-
-            for (int i = 0; i < n; i++)
-            {
-                timer.Restart();
-
-                func(files);
-
-                timer.Stop();
-                list.Add(timer.ElapsedMilliseconds);
-            }
-            return list.Average();
-        }
-
-        private static List<T> ReadToList<T>(IEnumerable<T> values)
-        {
-            var list = new List<T>();
-            foreach (T value in values)
-            {
-                list.Add(value);
-            }
-            return list;
-        }
-
-        private static LinkedList<T> ReadToLinkedList<T>(IEnumerable<T> values)
-        {
-            var list = new LinkedList<T>();
-            foreach (T value in values)
-            {
-                list.AddLast(value);
-            }
-            return list;
-        }
-
-
         /// <summary>Читаем из файла и записываем в лист</summary>
         /// <param name="path">путь к файлу</param>
         /// <returns></returns>
@@ -81,5 +42,58 @@ namespace WorkCollection.Task1
                     yield return read.ReadLine();
             yield break;
         }
+
+        /// <summary>Метод для подсчета среднего выполнения работы</summary>
+        /// <typeparam name="T">Обобщение</typeparam>
+        /// <param name="count">Количество итераций</param>
+        /// <param name="files">Источник</param>
+        /// <param name="func">Делегат куда записываем</param>
+        /// <returns>Среднее значение</returns>
+        static double Estimate<T>(int count,IEnumerable<T> files,Func<IEnumerable<T>,ICollection<T>> func)
+        {
+            var list = new List<long>(count);
+            var timer = new Stopwatch();
+            timer.Start();
+
+            for (int i = 0; i < count; i++)
+            {
+                timer.Restart();
+
+                func(files);
+
+                timer.Stop();
+                list.Add(timer.ElapsedMilliseconds);
+            }
+            return list.Average();
+        }
+
+        /// <summary>Записываем в List</summary>
+        /// <typeparam name="T">Обобщение</typeparam>
+        /// <param name="files">Источник</param>
+        /// <returns>Возвращаем List</returns>
+        private static List<T> ReadToList<T>(IEnumerable<T> files)
+        {
+            var list = new List<T>();
+            foreach (T value in files)
+            {
+                list.Add(value);
+            }
+            return list;
+        }
+
+        /// <summary>Записываем в LinkedList</summary>
+        /// <typeparam name="T">Обобщение</typeparam>
+        /// <param name="files">Источник</param>
+        /// <returns>Возвращаем LinkedList</returns>
+        private static LinkedList<T> ReadToLinkedList<T>(IEnumerable<T> files)
+        {
+            var list = new LinkedList<T>();
+            foreach (T value in files)
+            {
+                list.AddLast(value);
+            }
+            return list;
+        }
+
     }
 }
